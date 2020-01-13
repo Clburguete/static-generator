9f94faaf2d78d59bcf4f911e3b66aefd812d3a3f
Carlos Luis <clburguete@gmail.com>
2020-01-13 11:03:04 -0600

    feat: create config folder + change dist directory

--- config/webpack.js
+++ config/webpack.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: "./src/js/index.js",
   output: {
     filename: "index.js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve("dist")
   },
   plugins: [
     new HtmlWebpackPlugin({
