8b1224d0a1dc20e63fa090a6c864d29e49be6732
Carlos Luis <clburguete@gmail.com>
2020-01-18 21:37:13 -0600

    feat: rename files in config/

--- config/webpack.js
+++ config/webpack.js
@@ -1,35 +1,82 @@
+// Vendors
 const
-  CONFIG = require("./constants.js"),
-  { getHtmlConfig } = require("./utils.js"),
-  { CleanWebpackPlugin } = require("clean-webpack-plugin"),
-  globby = require("globby"),
-  path = require("path"),
-  MiniCssExtractPlugin = require("mini-css-extract-plugin");
+    path = require("path"),
+    { CleanWebpackPlugin } = require("clean-webpack-plugin"),
+    MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = async (env, argv) => {
-  const
-    { mode } = argv,
-    webpackConfig = CONFIG[mode],
-    isDevelopment = mode === 'development',
-    paths = await globby(["src/html/**"]),
-    HtmlWebpackConfig = getHtmlConfig(paths, isDevelopment);
-    
-  return {
-    entry: webpackConfig.entry,
-    output: webpackConfig.output,
-    devServer: {
-      openPage: "html"
+
+const webpackModule = {
+  rules: [
+    {
+      test: /\.js$/,
+      loader: "babel-loader",
+      query: {
+        presets: ["@babel/preset-env"],
+        plugins: [
+          "@babel/plugin-syntax-dynamic-import",
+          "@babel/plugin-transform-modules-amd"
+        ]
+      }
     },
-    plugins: [
-      ...webpackConfig.plugins,
-      ...HtmlWebpackConfig
-    ],
-    resolve: webpackConfig.resolve,
-    module: webpackConfig.module,
-    optimization: webpackConfig.optimization
-  };
+    {
+      test: /\.scss$/,
+      loader: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+    },
+    {
+      test: /\.(png|svg|jpg|gif)$/,
+      use: ["file-loader"]
+    },
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: ["file-loader"]
+    }
+  ]
+};
+const optimization = {
+    splitChunks: {
+        chunks: 'all'
+    }
+}
+const resolve = {
+    alias: {
+        "@css": path.resolve("src", "css"),
+        "@html": path.resolve("src", "html"),
+        "@js": path.resolve("src", "js")
+    }
+}
+const plugins = [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+        filename: "css/[name].css"
+    })
+]
+const commonConfig = {
+    module: webpackModule,
+    optimization,
+    resolve,
+    plugins
 }
 
-
-
-
+module.exports = {
+  production: {
+    entry: {
+      app: "./src/js/index.js"
+    },
+    output: {
+      path: path.resolve("dist"),
+      filename: "js/app.[contenthash].js"
+    },
+    ...commonConfig
+  },
+  development: {
+    entry: {
+      app: "./src/js/index.js",
+      contentTable: "./private/index.js"
+    },
+    output: {
+      path: path.resolve("dist"),
+      filename: "js/[name].js"
+    },
+    ...commonConfig
+  }
+};
