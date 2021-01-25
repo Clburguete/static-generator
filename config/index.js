892e0297d5380109fbe15671b452a3f84d355ba6
Carlos Luis <clburguete@gmail.com>
2021-01-25 19:35:26 +0100

    feat: clean old code

--- config/index.js
+++ config/index.js
@@ -1,32 +1,36 @@
 const
   CONFIG = require("./webpack.js"),
   { getHtmlConfig } = require("./utils.js"),
-  { CleanWebpackPlugin } = require("clean-webpack-plugin"),
-  globby = require("globby"),
-  path = require("path"),
-  MiniCssExtractPlugin = require("mini-css-extract-plugin");
+  globby = require("globby");
 
 module.exports = async (env, argv) => {
   const
     { mode } = argv,
-    webpackConfig = CONFIG[mode],
+    {
+      entry,
+      output,
+      plugins,
+      resolve,
+      module,
+      optimization
+    } = CONFIG[mode],
     isDevelopment = mode === 'development',
     paths = await globby(["src/html/**"]),
     HtmlWebpackConfig = getHtmlConfig(paths, isDevelopment);
-    
+
   return {
-    entry: webpackConfig.entry,
-    output: webpackConfig.output,
+    entry,
+    output,
     devServer: {
       openPage: "html"
     },
     plugins: [
-      ...webpackConfig.plugins,
+      ...plugins,
       ...HtmlWebpackConfig
     ],
-    resolve: webpackConfig.resolve,
-    module: webpackConfig.module,
-    optimization: webpackConfig.optimization
+    resolve,
+    module,
+    optimization
   };
 }
 
--- config/webpack.js
+++ config/webpack.js
@@ -1,61 +1,7 @@
 // Vendors
-const
-    path = require("path"),
-    { CleanWebpackPlugin } = require("clean-webpack-plugin"),
-    MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
 
-
-const webpackModule = {
-  rules: [
-    {
-      test: /\.js$/,
-      loader: "babel-loader",
-      query: {
-        presets: ["@babel/preset-env"],
-        plugins: [
-          "@babel/plugin-syntax-dynamic-import",
-          "@babel/plugin-transform-modules-amd"
-        ]
-      }
-    },
-    {
-      test: /\.scss$/,
-      loader: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
-    },
-    {
-      test: /\.(png|svg|jpg|gif)$/,
-      use: ["file-loader"]
-    },
-    {
-      test: /\.(woff|woff2|eot|ttf|otf)$/,
-      use: ["file-loader"]
-    }
-  ]
-};
-const optimization = {
-    splitChunks: {
-        chunks: 'all'
-    }
-}
-const resolve = {
-    alias: {
-        "@css": path.resolve("src", "css"),
-        "@html": path.resolve("src", "html"),
-        "@js": path.resolve("src", "js")
-    }
-}
-const plugins = [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-        filename: "css/[name].css"
-    })
-]
-const commonConfig = {
-    module: webpackModule,
-    optimization,
-    resolve,
-    plugins
-}
+const { config } = require("./config");
 
 module.exports = {
   production: {
@@ -66,7 +12,7 @@ module.exports = {
       path: path.resolve("dist"),
       filename: "js/app.[contenthash].js"
     },
-    ...commonConfig
+    ...config
   },
   development: {
     entry: {
@@ -77,6 +23,6 @@ module.exports = {
       path: path.resolve("dist"),
       filename: "js/[name].js"
     },
-    ...commonConfig
+    ...config
   }
 };
